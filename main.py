53529f80b17755c015479a46cda1ff00d7648de5
takashi-hash <69909903+takashi-hash@users.noreply.github.com>
2025-06-21 19:09:36 +0900

    Remove qt-material theme and update styles

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QFontDatabase, QFont
-from qt_material import apply_stylesheet
 from ui_main import MainWindow
 
 def main():
@@ -11,7 +10,6 @@ def main():
     app = QApplication(sys.argv)
     QFontDatabase.addApplicationFont("resources/fonts/Inter-Regular.ttf")
     app.setFont(QFont("Inter", 14))
-    apply_stylesheet(app, theme="resources/themes/easy_reading.xml")
     with open("resources/styles/style.qss") as f:
         app.setStyleSheet(app.styleSheet() + f.read())
     window = MainWindow()
