5582980ac1e180856feed1984a7fe5c69742ce04
takashi-hash <69909903+takashi-hash@users.noreply.github.com>
2025-06-21 21:10:25 +0900

    Fix Unicode decode error on Windows

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from ui_main import MainWindow
 def main():
     """GUI を起動するメイン関数"""
     app = QApplication(sys.argv)
-    with open("resources/styles/style.qss") as f:
+    with open("resources/styles/style.qss", encoding="utf-8") as f:
         app.setStyleSheet(app.styleSheet() + f.read())
     window = MainWindow()
     window.showMaximized()
