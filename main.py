1416f6d332b99b1f14b648708ab8522a23fc8f09
takashi-hash <69909903+takashi-hash@users.noreply.github.com>
2025-06-21 19:13:28 +0900

    Remove unused resources

--- main.py
+++ main.py
@@ -2,18 +2,13 @@
 
 import sys
 from PySide6.QtWidgets import QApplication
-from PySide6.QtGui import QFontDatabase, QFont
 from qt_material import apply_stylesheet
 from ui_main import MainWindow
 
 def main():
     """GUI を起動するメイン関数"""
     app = QApplication(sys.argv)
-    QFontDatabase.addApplicationFont("resources/fonts/Inter-Regular.ttf")
-    app.setFont(QFont("Inter", 14))
-    apply_stylesheet(app, theme="resources/themes/easy_reading.xml")
-    with open("resources/styles/style.qss") as f:
-        app.setStyleSheet(app.styleSheet() + f.read())
+    apply_stylesheet(app, theme="light_blue.xml")
     window = MainWindow()
     window.showMaximized()
     sys.exit(app.exec())
